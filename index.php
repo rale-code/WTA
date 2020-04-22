90fc51409378697e312cf1c5eb1e75082b0a99c7
rale-code <rale.todorovic2@gmail.com>
2020-04-22 21:48:59 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WTA</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://kit.fontawesome.com/d3d792014f.js" crossorigin="anonymous"></script>
+</head>
+	<body>
+		<?php include 'templates/nav.php'?>
+		<div class="picture">
+			<img src="https://wallpaperaccess.com/full/1687819.jpg">
+			<h1>Active players</h1>
+		</div>
+		
+	
+	</body>
+</html>
---
+++ templates/nav.php
@@ -0,0 +1,16 @@
+<header>
+		<img src="https://upload.wikimedia.org/wikipedia/commons/b/bf/WTA_logo_2010.svg" class="logo">
+		<nav>
+			<ul class="link">
+				<li><a class="cool-link" href="#">Home</a></li>
+				<li><a class="cool-link" href="#">Add player</a></li>
+				<li><a class="cool-link" href="#">Contact</a></li>
+			</ul>
+		</nav>
+		<div class="search-box">
+			<input type="text" name="search" class="search-txt" placeholder="Search...">
+			<a href="#" class="search-btn">
+				<i class="fas fa-search"></i>
+			</a>
+		</div>	
+</header> 
